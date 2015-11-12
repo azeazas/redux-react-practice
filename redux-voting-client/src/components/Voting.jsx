5646de9cf3f7817daccbed56ed47461cd33ccb10
Alexa Zeazas <student@epicodus.com>
2015-11-12 15:07:20 -0800

    Dispatch Vote action from React.

--- redux-voting-client/src/components/Voting.jsx
+++ redux-voting-client/src/components/Voting.jsx
@@ -3,9 +3,10 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {connect} from 'react-redux';
 import Winner from './Winner';
 import Vote from './Vote';
+import * as actionCreators from '../action_creators';
 
 export const Voting = React.createClass({
-  mixins:[PureRenderMixin],
+  mixins: [PureRenderMixin],
   render: function() {
     return <div>
       {this.props.winner ?
@@ -18,8 +19,12 @@ export const Voting = React.createClass({
 function mapStateToProps(state) {
   return {
     pair: state.getIn(['vote', 'pair']),
+    hasVoted: state.get('hasVoted'),
     winner: state.get('winner')
   };
 }
 
-export const VotingContainer = connect(mapStateToProps)(Voting);
+export const VotingContainer = connect(
+  mapStateToProps,
+  actionCreators
+)(Voting);
